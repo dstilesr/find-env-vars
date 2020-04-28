cb6d91031d033417bc92308e6573a4bb3d1009d5
dstilesr <d.stiles.r@hotmail.com>
2020-04-28 14:16:07 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="finders",
+    version="0.0.1",
+    packages=find_packages(),
+    description="",
+    author="David Stiles Rosselli",
+    author_email="d.stiles.r@hotmail.com"
+)
