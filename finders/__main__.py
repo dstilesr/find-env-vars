b1dd6ee5f85a8f70af46ce0272c1399d2eb02e1e
dstilesr <d.stiles.r@hotmail.com>
2021-03-26 18:51:47 -0500

    Use fire to wrap function for CLI API

--- finders/__main__.py
+++ finders/__main__.py
@@ -1,21 +1,23 @@
 import os
-from sys import argv, exit
+import fire
 from .env_finder import EnvFinder
 
-# Validate arguments
-if len(argv) < 2:
-    print("ERROR")
-    print("USAGE: ")
-    print("python -m finders <directory path>")
-    exit(1)
 
-if not os.path.isdir(argv[1]):
-    print("ERROR")
-    print("Invalid path! Must point to a directory.")
-    exit(1)
+def find_env_variables(source_dir: str = "."):
+    """
+    Find the names of all environment variables used in a python project and
+    save the results to a .env.example and a json file.
+    :param source_dir: Directory of the project to be searched.
+    :return: None
+    """
+    if not os.path.isdir(source_dir):
+        raise FileNotFoundError(
+            "The path '%s' is not a directory!" % source_dir
+        )
 
-# Find and dump matches
-finder = EnvFinder(argv[1])
-finder.dump_matches_all()
-print("INFO: DONE!")
-exit(0)
+    finder = EnvFinder(source_dir)
+    finder.dump_matches_all()
+
+
+# Launch with Fire
+fire.Fire(find_env_variables)
